827c6686f5f734e5261ba9f8f95ef8171e31d066
Fugazzii <sichinavailia@gmail.com>
2023-11-21 00:45:16 +0400

    Implemented queue

--- queue/queue.c
+++ queue/queue.c
@@ -1,30 +1,38 @@
 #include "queue.h"
 
-void QueueNew(Queue * q, size_t elemSize){
-    assert(elemSize > 0);
-    q->base = malloc(INIT_ALLOC_LEN);
-    assert(q->base != NULL);
+void QueueNew(Queue* q, size_t elem_size){
+    q->base = malloc(elem_size * INIT_ALLOC_LEN);
     q->alloc_len = INIT_ALLOC_LEN;
-    q->elem_size = elemSize;
+    q->elem_size = elem_size;
     q->log_len = 0;
-
 }
-void QueueDispose(Queue * q){
+
+void QueueDispose(Queue* q){
+    assert(q != NULL);
     free(q->base);
 }
-void QueueEnque(Queue * q, void * elemAddr){
-    if(q->log_len == q->alloc_len){
-        q->alloc_len *=2;
-        realloc(q->base, q->alloc_len);
-        assert(q->base != NULL);
+
+void QueueEnque(Queue* q, void* elem_addr){
+    if(q->log_len == q->alloc_len) {
+        q->alloc_len *= 4;
+        q->base = realloc(q->base, q->alloc_len);
     }
-    memcpy((char *)q->base + q->elem_size*q->log_len, elemAddr, q->elem_size);
 
+    memcpy(
+        (char*)q->base + q->log_len * q->elem_size,
+        elem_addr, 
+        q->elem_size
+    );
+    q->log_len += 1;
 }
-void QueueDeque(Queue * q, void * elemAddr){
-    assert(q->log_len > 0);
-    q->log_len--;
-    memcpy(elemAddr, q->base, q->elem_size);
-    memmove(q->base, (char*)q->base + q->elem_size, q->elem_size*q->log_len);
 
+void QueueDeque(Queue * q, void * elem_addr){
+    assert(q->log_len > 0);
+    memcpy(elem_addr, q->base, q->elem_size);
+    memmove(
+        q->base,
+        (char*)q->base + q->elem_size,
+        (q->log_len - 1) * q->elem_size
+    );    
+    q->log_len -= 1;
 }
--- queue/queue.h
+++ queue/queue.h
@@ -7,16 +7,15 @@
 
 #define INIT_ALLOC_LEN 4
 
-typedef struct 
-{
+typedef struct  {
     size_t elem_size;
     size_t alloc_len;
     size_t log_len;
-    void *base;
-}Queue;
-void QueueNew(Queue * q, size_t elemSize);
+    void* base;
+} Queue;
+void QueueNew(Queue * q, size_t elem_size);
 void QueueDispose(Queue * q);
-void QueueEnque(Queue * q, void * elemAddr);
-void QueueDeque(Queue * q, void * elemAddr);
+void QueueEnque(Queue * q, void * elem_addr);
+void QueueDeque(Queue * q, void * elem_addr);
 
 #endif
